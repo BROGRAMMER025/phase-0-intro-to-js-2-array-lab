4b9fc7830b3679dd228e87146c11355920668ca5
Allan Kinyua Murigi <allan.murigi@student.moringaschool.com>
2023-10-18 21:09:17 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    const newCatsArray = [...cats,"Broom"]
+    return newCatsArray
+}
+ appendCat(cats)
+ function prependCat(name){
+    const otherCats = ["Arnold", ...cats]
+    return otherCats
+ }
+ appendCat(cats)
+ function removeLastCat(){
+    const removeCat = cats.slice(0, -1)
+    return removeCat  
+ }
+ removeLastCat()
+
+ function removeFirstCat(){
+    const firstCatRemove = cats.slice(1)
+    return firstCatRemove
+ }
+ removeFirstCat()
